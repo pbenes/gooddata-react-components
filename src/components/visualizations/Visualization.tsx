81a95a9573542f91af83e4c2110900584cd72f9b
Petr Benes <petr.benes@gooddata.com>
2018-03-26 16:36:22 +0200

    RELATED: FET-57 Move charts to react-components

---
+++ src/components/visualizations/Visualization.tsx
@@ -0,0 +1,140 @@
+// (C) 2007-2018 GoodData Corporation
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import * as invariant from 'invariant';
+import noop = require('lodash/noop');
+import isEqual = require('lodash/isEqual');
+import isFunction = require('lodash/isFunction');
+import omitBy = require('lodash/omitBy');
+import * as Highcharts from 'highcharts';
+
+// TODO: will be ported later
+import {
+    TableTransformation
+} from '@gooddata/indigo-visualizations';
+
+import {
+    Execution
+} from '@gooddata/typings';
+
+import {
+    isTable,
+    isChartSupported,
+    stringifyChartTypes
+} from './utils/common';
+
+import { IChartConfig } from './chart/Chart';
+import ChartTransformation, { IExecutionRequest, IDrillableItems, renderHighCharts } from './chart/ChartTransformation';
+
+export interface IVisualizationProps {
+    height: number;
+    width: number;
+    config: IChartConfig;
+    numericSymbols: string[];
+
+    executionRequest: IExecutionRequest;
+    executionResponse: Execution.IExecutionResponse;
+    executionResult: Execution.IExecutionResult;
+    drillableItems: IDrillableItems[];
+
+    onFiredDrillEvent(): void;
+    afterRender(): void;
+    onDataTooLarge(): void;
+    onNegativeValues(): void;
+    onLegendReady(): void;
+}
+
+export class Visualization extends React.Component<IVisualizationProps, null> {
+    public static propTypes = {
+        config: PropTypes.shape({
+            type: PropTypes.string.isRequired
+        }).isRequired,
+        onFiredDrillEvent: PropTypes.func,
+        numericSymbols: PropTypes.array,
+        afterRender: PropTypes.func
+    };
+
+    public static defaultProps = {
+        numericSymbols: [] as string[],
+        onFiredDrillEvent: noop,
+        afterRender: noop
+    };
+
+    constructor(props: IVisualizationProps) {
+        super(props);
+
+        this.setNumericSymbols(this.props);
+    }
+
+    public componentWillReceiveProps(nextProps: IVisualizationProps) {
+        this.setNumericSymbols(nextProps);
+    }
+
+    public shouldComponentUpdate(nextProps: IVisualizationProps) {
+        return !isEqual(omitBy(this.props, isFunction), omitBy(nextProps, isFunction));
+    }
+
+    public setNumericSymbols(props: IVisualizationProps) {
+        const { numericSymbols } = props;
+
+        if (numericSymbols && numericSymbols.length) {
+            Highcharts.setOptions({
+                lang: {
+                    numericSymbols
+                }
+            });
+        }
+    }
+
+    public render(): JSX.Element {
+        const visType = this.props.config.type;
+
+        if (isTable(visType)) {
+            return (
+                <TableTransformation {...this.props} />
+            );
+        }
+
+        if (isChartSupported(visType)) {
+            const {
+                height,
+                width,
+                config,
+                executionRequest,
+                executionResponse,
+                executionResult,
+                drillableItems,
+                onFiredDrillEvent,
+                afterRender,
+                onDataTooLarge,
+                onNegativeValues,
+                onLegendReady
+            } = this.props;
+
+            return (
+                <ChartTransformation
+                    height={height}
+                    width={width}
+                    config={config}
+                    drillableItems={drillableItems}
+
+                    executionRequest={executionRequest}
+                    executionResponse={executionResponse}
+                    executionResult={executionResult}
+
+                    afterRender={afterRender}
+                    onFiredDrillEvent={onFiredDrillEvent}
+                    onDataTooLarge={onDataTooLarge}
+                    onNegativeValues={onNegativeValues}
+                    onLegendReady={onLegendReady}
+                    renderer={renderHighCharts}
+                />
+            );
+        }
+
+        invariant(isChartSupported(visType),
+            `Unknown visualization type: ${visType}. Supported visualization types: ${stringifyChartTypes()}`);
+
+        return null;
+    }
+}
---
+++ src/components/visualizations/chart/test/Chart.spec.tsx
@@ -0,0 +1,56 @@
+// (C) 2007-2018 GoodData Corporation
+import * as Highcharts from 'highcharts';
+import * as React from 'react';
+import { mount } from 'enzyme';
+
+import Chart from '../Chart';
+
+jest.mock('highcharts', () => {
+    return {
+        Chart: ({}, callback: any) => {
+            callback();
+        }
+    };
+});
+
+describe('Chart', () => {
+    function createComponent(props: any = {}) {
+        return mount(<Chart config={{}} {...props} />);
+    }
+
+    it('should render highcharts', () => {
+        const spy = jest.spyOn(Highcharts, 'Chart');
+        const wrapper = createComponent();
+        const component: any = wrapper.instance();
+        expect(component.chart).toBeTruthy();
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should rerender highcharts on props change', () => {
+        const createChartSpy = jest.spyOn(Chart.prototype, 'createChart');
+        const wrapper = createComponent();
+
+        expect(createChartSpy).toHaveBeenCalledTimes(1);
+
+        wrapper.setProps({ config: { foo: 'bar' } });
+        expect(createChartSpy).toHaveBeenCalledTimes(2);
+
+        createChartSpy.mockReset();
+        createChartSpy.mockRestore();
+    });
+
+    it('should call callback on componentDidMount', () => {
+        const callback = jest.fn();
+        createComponent({ callback });
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call destroy callback on componentWillUnmount', () => {
+        const wrapper = createComponent();
+        const component: any = wrapper.instance();
+        component.chart.destroy = jest.fn();
+
+        component.componentWillUnmount();
+        expect(component.chart.destroy).toHaveBeenCalledTimes(1);
+    });
+});
