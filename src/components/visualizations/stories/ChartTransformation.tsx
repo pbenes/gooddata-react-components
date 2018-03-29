b8cd957ba789a4de315d4ae0fe63662464ec57fb
Petr Benes <petr.benes@gooddata.com>
2018-03-29 09:28:14 +0200

    storybook

--- src/components/visualizations/stories/ChartTransformation.tsx
+++ src/components/visualizations/stories/ChartTransformation.tsx
@@ -1,5 +1,5 @@
 // (C) 2007-2018 GoodData Corporation
-import React from 'react';
+import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { screenshotWrap } from '@gooddata/test-storybook';
