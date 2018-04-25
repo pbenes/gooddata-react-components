710113abee6d56083ae41eb0e2c058bfe981afbc
Peter Lacko <peter.lacko@gooddata.com>
2018-04-25 13:40:08 +0200

    RELATED: FET-69 Add TS types

--- src/components/visualizations/Visualization.tsx
+++ src/components/visualizations/Visualization.tsx
@@ -5,7 +5,6 @@ import isEqual = require('lodash/isEqual');
 import noop = require('lodash/noop');
 import isFunction = require('lodash/isFunction');
 import omitBy = require('lodash/omitBy');
-import { Highcharts, IChartConfig } from './chart/Chart';
 
 // TODO: will be ported later
 import {
@@ -22,7 +21,11 @@ import {
     stringifyChartTypes
 } from './utils/common';
 
-import ChartTransformation, { IExecutionRequest, IDrillableItems, renderHighCharts } from './chart/ChartTransformation';
+import { Highcharts, IChartConfig } from './chart/Chart';
+
+import { IDrillableItem } from '../../interfaces/DrillEvents';
+
+import ChartTransformation, { IExecutionRequest, renderHighCharts } from './chart/ChartTransformation';
 
 export interface IVisualizationProps {
     height: number;
@@ -31,9 +34,9 @@ export interface IVisualizationProps {
     numericSymbols: string[];
 
     executionRequest: IExecutionRequest;
-    executionResponse: Execution.IExecutionResponse;
-    executionResult: Execution.IExecutionResult;
-    drillableItems: IDrillableItems[];
+    executionResponse: Execution.IExecutionResponse['executionResponse'];
+    executionResult: Execution.IExecutionResult['executionResult'];
+    drillableItems: IDrillableItem[];
 
     onFiredDrillEvent(): void;
     afterRender(): void;
