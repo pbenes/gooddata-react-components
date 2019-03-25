b73ad711fc00d64eb4c16619ea3c62412e21c15c
Petr Benes <petr.benes@gooddata.com>
2019-03-25 09:53:57 +0100

    Add copy

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,11 @@
 
 _common-build(){
     rm -rf dist
-    mkdir dist
+    mkdir -p dist/internal/
     cp -rf src/translations/ dist/translations/
+    cp -rf src/internal/translations/ dist/internal/translations/
+    cp -rf src/internal/assets/ dist/internal/assets/
+    cp -rf src/internal/styles/ dist/internal/styles/
     node-sass --importer node_modules/node-sass-magic-importer/dist/cli.js -o styles/css styles/scss
 }
 
